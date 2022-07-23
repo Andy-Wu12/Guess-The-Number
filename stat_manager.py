c0180ea9467ab61b9ccecefeb1fcbfab0395a032
Andy Wu <awpats12@gmail.com>
2022-07-22 23:55:14 -0400

    Finished #9, partially #8. prettyPrint now provides formatted and detailed statistics for each tracked variable

--- stat_manager.py
+++ stat_manager.py
@@ -2,18 +2,32 @@ import json
 
 import util
 
+varNameToStatName = {
+        "wins": "Games Won",
+        "losses": "Games Lost",
+        "num_guesses": "Total # of valid guesses",
+        "num_first_correct": "Number of correct first guesses",
+        "fastest_win_in_turns": "Lowest # of tries used to win",
+        "slowest_win_in_turns": "Highest # of tries used to win",
+        "highest_difficulty_win": "Highest difficulty beaten",
+        "num_easy_wins": "Wins on easy mode",
+        "num_med_wins": "Wins on medium mode",
+        "num_hard_wins": "Wins on hard mode"
+    }
+
 class StatManager:
+
     def __init__(self):
         # Counters
         self.wins = 0
         self.losses = 0
         self.num_guesses = 0
-        self.num_first_guesses_correct = 0
+        self.num_first_correct = 0
 
         # Miscellaneous
         self.fastest_win_in_turns = -1
         self.slowest_win_in_turns = -1
-        self.highest_difficulty_beaten = ""
+        self.highest_difficulty_win = ""
         self.num_easy_wins = 0
         self.num_med_wins = 0
         self.num_hard_wins = 0
@@ -23,7 +37,7 @@ class StatManager:
 
     def prettyPrint(self):
         print("\nYour game statistics")
-        util.printWithBorder(self.__str__())
+        util.printWithBorder("\n".join(f"{varNameToStatName[stat]}: {value}" for stat, value in self.__dict__.items()))
 
     def save(self):
         pass
