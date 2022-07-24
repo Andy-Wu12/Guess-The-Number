9eee2c7bba4f34e1f755eb096a1ba354fb29b356
Andy Wu <33229522+Andy-Wu12@users.noreply.github.com>
2022-07-24 13:12:57 -0400

    Merging test branch after updating ci tests (#18)
    * Added simple test for saves, testing ci setup (#15, #16)
    
    * Create python-app.yml
    
    * Refactored GameManager to be in game.py itself to allow for testing, Implemented two tests (#15) for saving functionality
    
    * Update python-app.yml to run python3 - m pytest
    
    instead of running pytest as a standalone executable.
    This should fix the issue of modules not being found
    
    * Update python-app.yml
    
    * Fixed codiga design issue with initializing variables outside of class __init__

---
+++ tests/test_stat_save.py
@@ -0,0 +1,32 @@
+import os.path
+
+import pytest
+
+from game import Game
+from stat_manager import StatManager
+
+class TestSaves:
+    def __init__(self):
+        self.game = None
+        self.stat_manager = None
+
+    @pytest.fixture(autouse=True)
+    def setup_and_teardown(self):
+        # Setup code needed before tests run
+        self.game = Game()
+        self.stat_manager = StatManager()
+        # Actual tests run while yielding
+        yield
+        # Teardown by deleting SAVE FILE after each test
+        if os.path.exists(self.game.SAVEFILE_NAME):
+            from os import remove
+            remove(self.game.SAVEFILE_NAME)
+
+    # TEST: Initial persistent file should not exist on very first startup
+    def test_savefile_missing_on_first_start(self):
+        assert not os.path.exists(self.game.SAVEFILE_NAME)
+
+    # TEST: Ensure persistent file exists after saving
+    def test_savefile_created_on_save(self):
+        self.game.saveGameStats()
+        assert os.path.exists(self.game.SAVEFILE_NAME)
