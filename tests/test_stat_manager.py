fa87e99f552f04f6912e32d62384b9baa56239ee
Andy Wu <awpats12@gmail.com>
2022-07-24 23:34:50 -0400

    Fixed issue with using pytest's tmp_path incorrectly. Tests for (#16) no longer throw filenotfound error due to test setup function

--- tests/test_stat_manager.py
+++ tests/test_stat_manager.py
@@ -14,23 +14,22 @@ BAD_JSON = {'randomField': 'hi', 'secondField': 'world'}
 GOOD_JSON = StatManager().__dict__
 NO_JSON = "random text that isn't json"
 
+def create_test_files(tmp_path):
+    temp_dir = tmp_path / "test_root"
+    temp_dir.mkdir()
+    bad_json_file = temp_dir / "bad_json.txt"
+    bad_json_file.write_text(json.dumps(BAD_JSON))
+    good_json_file = temp_dir / "good_json.txt"
+    good_json_file.write_text(json.dumps(GOOD_JSON))
+    no_json = temp_dir / "no_json.txt"
+    no_json.write_text(NO_JSON)
+
 class TestStatManager:
-    @pytest.fixture(autouse=True)
-    def setup_and_teardown(self, tmp_path):
+    @pytest.fixture(autouse=True, scope='function')
+    def setup(self, tmp_path):
         # Setup code needed before tests run
         self.stat_manager = StatManager()
-        temp_dir = tmp_path / "test_root"
-
-        bad_json_file = temp_dir / "bad_json.txt"
-        bad_json_file.write_text(json.dumps(BAD_JSON))
-        good_json_file = temp_dir / "good_json.txt"
-        good_json_file.write_text(json.dumps(GOOD_JSON))
-        no_json = temp_dir / "no_json.txt"
-        no_json.write_text(NO_JSON)
-
-        # Actual tests run while yielding
-        yield
-        # Teardown by deleting SAVE FILE after each test
+        create_test_files(tmp_path)
 
     # TEST: Ensure requested filename should load if it contains JSON
     def test_valid_filename_load(self):
